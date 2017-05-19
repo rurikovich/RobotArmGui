07ab9471349b81cde227558deb3a41411b036ce3
rurik <urrast@yandex.ru>
2017-05-19 22:53:06 +0300

    actions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,8 +2,35 @@ package sample;
 
 public class Controller {
 
-    public  void test1(){
-        System.out.println("test1");
+    public  void up(){
+        System.out.println("up");
     }
 
+    public  void down(){
+        System.out.println("down");
+    }
+
+    public  void left(){
+        System.out.println("left");
+    }
+
+    public  void right(){
+        System.out.println("right");
+    }
+
+    public  void forward(){
+        System.out.println("forward");
+    }
+
+    public  void back(){
+        System.out.println("back");
+    }
+
+    public  void gripperClose(){
+        System.out.println("gripperClose");
+    }
+
+    public  void gripperOpen(){
+        System.out.println("gripperOpen");
+    }
 }
